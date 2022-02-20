3dabcd3f6400d29209e71e47efd686c3ecec2835
ViniciusCavalcante2000 <viniciuscavalcanterj@gmail.com>
2022-02-20 10:23:10 -0300

    Acrescimo botoes AssistirAgora e MaisInformacoes. Acrscentado Plno de fundo com degrade. Uso do background-size cover para ajustas o conteudo na caixa. Criacao de alguma classes no html para serem usadas posteriormente

---
+++ Push.cmd
@@ -0,0 +1,5 @@
+git add *
+
+git commit -m "Acrescimo botoes AssistirAgora e MaisInformacoes. Acrscentado Plno de fundo com degrade. Uso do background-size cover para ajustas o conteudo na caixa. Criacao de alguma classes no html para serem usadas posteriormente"
+
+git push origin main
