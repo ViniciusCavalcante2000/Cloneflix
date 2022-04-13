b06d81e985ea711dd1c3ecfd00a1d31d89299292
ViniciusCavalcante2000 <viniciuscavalcanterj@gmail.com>
2022-04-13 07:51:25 -0300

    Mudei a fonte do Titulo Rarefeito para 'Trebuchet MS'. Modifica├º├Áes nos bot├Áes 'Assisitr Agora e Mais Informa├º├Áes', que foram: Inversao de cores ao passar o cursor com transi├º├úo gradual de 300ms, Foram usados icones de play e info do https://kit.fontawesome.com.

--- Push.cmd
+++ Push.cmd
@@ -1,5 +1,5 @@
 git add *
 
-git commit -m "Imagem de fundo"
+git commit -m "Mudei a fonte do Titulo Rarefeito para 'Trebuchet MS'. Modificações nos botões 'Assisitr Agora e Mais Informações', que foram: Inversao de cores ao passar o cursor com transição gradual de 300ms, Foram usados icones de play e info do https://kit.fontawesome.com. "
 
 git push origin main
